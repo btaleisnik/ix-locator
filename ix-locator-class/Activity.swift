ed35d29cd501ac7a64112112b606e4eae2ac9dbb
Brandon Taleisnik <btaleisnik@gmail.com>
2017-07-04 16:30:12 +0200

    Added activity cells

---
+++ ix-locator-class/Activity.swift
@@ -0,0 +1,22 @@
+//
+//  Activity.swift
+//  ix-locator-class
+//
+//  Created by Brandon Taleisnik on 7/4/17.
+//  Copyright © 2017 Brandon Taleisnik. All rights reserved.
+//
+
+import Foundation
+
+class Activity {
+    
+    var name: String?
+    var description: String?
+    
+    init(name: String?, description: String?) {
+        self.name = name
+        self.description = description
+    }
+    
+    
+}
